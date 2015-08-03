c0bca2d1792f937644c946beb23204dda2949600
Shramee <shramee.srivastav@gmail.com>
2015-08-03 19:11:03 +0530

    Update api activate notice

--- woo-customizer.php
+++ woo-customizer.php
@@ -21,17 +21,6 @@ if (is_woocommerce_activated()){}
 
 	require_once('inc/functions.php');
 	require_once('inc/actions.php');
-	function woo_custom_wp_admin_scripts() {
-		wp_enqueue_script(
-		'woo_custom-customizer-menu-script',
-		plugins_url(). '/woo-customizer/js/customizr-menu.js',
-		array( 'jquery'),
-		'0.3.0',
-		true
-		);
-		wp_enqueue_style('woo_custom-customizer-menu-script', plugins_url(). '/cx-button-customizer/css/customizr-menu.css');
-	}
-	add_action( 'admin_enqueue_scripts', 'woo_custom_wp_admin_scripts' );
 
 //CX API
 require 'pp-cx/class-pp-cx-init.php';
